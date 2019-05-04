102f6eee4a1cce0ca6b43c5b00a604776d3bcbdc
WisterViolet <fujianifan@gmail.com>
2019-05-05 08:14:56 +0900

    .cpp,.hの骨格を作った

--- remoconSerial.cpp
+++ remoconSerial.cpp
@@ -1,2 +1,16 @@
 #include "Arduino.h"
 #include "remoconSerial.h"
+void (*Dataf[8])(int) = {
+    Data0,
+    Data1,
+    Data2,
+    Data3,
+    Data4,
+    Data5,
+    Data6,
+    Data7,
+}
+
+void DRoute(byte d){
+    Dataf[d & 0b111](d >> 3);
+}
---
+++ remoconSerial.h
@@ -0,0 +1,13 @@
+#ifndef REMOCONSERIAL_H
+#define REMOCONSERIAL_H
+struct remoconData;
+void Data0(int);
+void Data1(int);
+void Data2(int);
+void Data3(int);
+void Data4(int);
+void Data5(int);
+void Data6(int);
+void Data7(int);
+void DRoute(int);
+#endif
