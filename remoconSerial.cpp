fcd24e12c1e878964034ce842287230a3acff134
WisterViolet <fujianifan@gmail.com>
2019-05-11 16:54:28 +0900

    exampleテスト済み

--- remoconSerial.cpp
+++ remoconSerial.cpp
@@ -1,6 +1,57 @@
 #include "Arduino.h"
 #include "remoconSerial.h"
-void (*SDataf[8])(int) = {
+
+
+
+void Data0(byte d,remoconData *R){
+    R->A = !!bitRead(d,4);
+    R->B = !!bitRead(d,3);
+    R->X = !!bitRead(d,2);
+    R->Y = !!bitRead(d,1);
+    R->Start = !!bitRead(d,0);
+}
+
+void Data1(byte d,remoconData *R){
+    R->LEFT = !!bitRead(d,4);
+    R->DOWN = !!bitRead(d,3);
+    R->UP = !!bitRead(d,2);
+    R->RIGHT = !!bitRead(d,1);
+    R->Select = !!bitRead(d,0);
+}
+
+void Data2(byte d,remoconData *R){
+    R->R1 = !!bitRead(d,4);
+    R->R2 = !!bitRead(d,3);
+    R->L1 = !!bitRead(d,2);
+    R->L2 = !!bitRead(d,1);
+    R->Home = !!bitRead(d,0);
+}
+
+void Data3(byte d,remoconData *R){
+    R->R3 = !!bitRead(d,5);
+    R->L3 = !!bitRead(d,4);
+    R->RF = !!bitRead(d,3);
+    R->LF = !!bitRead(d,2);
+    R->Start = !!bitRead(d,1);
+}
+
+void Data4(byte d,remoconData *R){
+    R->StickL = d;
+}
+
+void Data5(byte d,remoconData *R){
+    R->StickR = d;
+}
+
+void Data6(byte d,remoconData *R){
+    R->AddA = d;
+}
+
+void Data7(byte d,remoconData *R){
+    R->AddB = d;
+}
+
+void (*SDataf[8])(byte, remoconData *R) = {
     Data0,
     Data1,
     Data2,
@@ -10,7 +61,6 @@ void (*SDataf[8])(int) = {
     Data6,
     Data7,
 };
-
-void DRoute(byte d){
-    SDataf[d & 0b111](d >> 3);
+void DRoute(byte d, remoconData *R){
+    SDataf[d & 0b111](d >> 3, R);
 }
--- remoconSerial.h
+++ remoconSerial.h
@@ -1,13 +1,38 @@
 #ifndef REMOCONSERIAL_H
 #define REMOCONSERIAL_H
-struct remoconData;
-void Data0(int);
-void Data1(int);
-void Data2(int);
-void Data3(int);
-void Data4(int);
-void Data5(int);
-void Data6(int);
-void Data7(int);
-void DRoute(byte);
+struct remoconData{
+    bool A = 0;
+    bool B = 0;
+    bool X = 0;
+    bool Y = 0;
+    bool UP = 0;
+    bool DOWN = 0;
+    bool RIGHT = 0;
+    bool LEFT = 0;
+    bool R1 = 0;
+    bool R2 = 0;
+    bool R3 = 0;
+    bool L1 = 0;
+    bool L2 = 0;
+    bool L3 = 0;
+    bool Start = 0;
+    bool Select = 0;
+    bool Home = 0;
+    bool RF = 0;
+    bool LF = 0;
+    bool P = 0;
+    byte StickL = 0;
+    byte StickR = 0;
+    byte AddA = 0;
+    byte AddB = 0;
+};
+void Data0(byte, remoconData*);
+void Data1(byte, remoconData*);
+void Data2(byte, remoconData*);
+void Data3(byte, remoconData*);
+void Data4(byte, remoconData*);
+void Data5(byte, remoconData*);
+void Data6(byte, remoconData*);
+void Data7(byte, remoconData*);
+void DRoute(byte, remoconData*);
 #endif
