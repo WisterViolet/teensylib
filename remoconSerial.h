63654017f3e543f16c9025f321ff69521f5f92d0
WisterViolet <fujianifan@gmail.com>
2019-07-06 21:07:26 +0900

    remoconSerial.hでの説明

--- remoconSerial.h
+++ remoconSerial.h
@@ -1,5 +1,11 @@
 #ifndef REMOCONSERIAL_H
 #define REMOCONSERIAL_H
+/*
+* RemoconSerialライブラリ
+* 概要: リモコン->無線機器(送信)~>無線機器(受信)->(ここで値を受け取るためのもの)
+* 作成: WisterViolet(@wister_fl)
+* ボタン割り当てはREADME.mdに置いておきます.
+*/
 struct remoconData{
     bool A = 0;
     bool B = 0;
@@ -17,13 +23,13 @@ struct remoconData{
     bool L3 = 0;
     bool Start = 0;
     bool Select = 0;
-    bool Home = 0;
+    bool Home = 0; // Nintendo側でいうHOME,DualShock側でいうANALOG
     bool RF = 0;
     bool LF = 0;
-    bool P = 0;
-    byte StickL = 0;
-    byte StickR = 0;
-    byte AddA = 0;
+    bool P = 0; // チェックサム(LF XOR RF).通信のノイズでLFとRFが変わるのを防ぐはず.
+    byte StickL = 0; // Lスティックの角度(0~31)
+    byte StickR = 0; // Rスティックの角度(0~31)
+    byte AddA = 0; // もしコントローラーの外付けとか.PS4のDualShockだとタッチパッドあたりがいけるかも?
     byte AddB = 0;
 };
 void Data0(byte, remoconData*);
@@ -35,4 +41,8 @@ void Data5(byte, remoconData*);
 void Data6(byte, remoconData*);
 void Data7(byte, remoconData*);
 void DRoute(byte, remoconData*);
+/*
+* Data0~7に関しては,気にする必要はない.
+* DRouteは(受信したデータ, remoconData構造体(上のやつ))の順に引数を取り,決まった場所にデータを入れてくれる.
+*/
 #endif
